7709b25c1688b8b9bc83da01f7bf83e66d7a95d3
Mike Harper <mike@mcharper.com>
2018-04-15 16:30:15 +0100

    Deal with issue of missing picsum images

--- src/modules/App/components/Visualiser.js
+++ src/modules/App/components/Visualiser.js
@@ -3,16 +3,9 @@ import PropTypes from "prop-types";
 import CrossfadeImage from "react-crossfade-image";
 
 const Visualiser = ({ changeNumber }) => {
-  if(changeNumber % 2 === 0) {
     return (
-        <CrossfadeImage className="visual" duration="2000" src={"https://picsum.photos/800/300?image=" + changeNumber} />
+        <CrossfadeImage className="visual" duration="2000" src={"https://picsum.photos/800/300?random&" + changeNumber} />
     );
-  }
-  else {
-    return (
-      <CrossfadeImage className="visual" duration="2000" src={"https://picsum.photos/800/300?image=" + changeNumber} />
-    );
-  }
 };
 
 Visualiser.propTypes = {
